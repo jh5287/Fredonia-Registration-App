98ff8b62acaf9899fff4b3bf61f8262f4c4ea632
Jared Russell <jaredhrussell2016@gmail.com>
2024-02-03 11:59:12 -0500

    Add tailwind css to app

--- fred-registration-app/src/app/page.js
+++ fred-registration-app/src/app/page.js
@@ -1,18 +1,69 @@
-import React from 'react';
-import styles from './Page.module.css'; // Import CSS module
+'use client'
+import React, {useState} from 'react';
+import styles from './page.module.css'; // Import CSS module
+import CGPA from '@/components/CGPA';
+import UserInfo from '@/components/UserInfo';
 
 const Page = () => {
+  const [isSpanning, setIsSpanning] = useState(true);
+
+  const handleClose = () => {
+    setIsSpanning(false);
+  };
+
+  const handlePutBack = () => {
+    setIsSpanning(true);
+  };
   return (
+    <div>
     <div className={styles.container}> {/* Apply container class */}
-      {/* Render 8 evenly spaced divs */}
-      <div className={styles.item}>1</div>
-      <div className={styles.item}>2</div>
-      <div className={styles.item}>3</div>
-      <div className={styles.item}>4</div>
-      <div className={styles.item}>5</div>
-      <div className={styles.item}>6</div>
-      <div className={styles.item}>7</div>
-      <div className={styles.item}>8</div>
+      {/* First div spans across 3 columns */}
+      <CGPA classname={`${styles.item} ${styles['CGPA']}`} />{/* Second div spans across 3 columns */}
+      {/* Remaining divs */}
+      <UserInfo classname={`${styles.item} ${styles['user-info']}`} />
+
+      <div className={styles.item}>
+        <div className={styles['item-header']}>
+          Semester 1
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles['item-header']}>
+          Semester 2
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles['item-header']}>
+          Semester 3
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles['item-header']}>
+          Semester 4
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles['item-header']}>
+          Semester 5
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles['item-header']}>
+          Semester 6
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles['item-header']}>
+          Semester 7
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles['item-header']}>
+          Semester 8
+        </div>
+      </div>
+    </div>
+    <div className='text-3xl font-bold underline'>Hello world</div>
     </div>
   );
 };
