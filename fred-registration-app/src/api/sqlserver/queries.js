8663af1fa7edd3eab2f7c54f96f8314e835bef05
Camron Walsh <camronwalsh@gmail.com>
2024-02-18 17:38:26 -0500

    Database Driven Catalog Rendering

--- fred-registration-app/src/api/sqlserver/queries.js
+++ fred-registration-app/src/api/sqlserver/queries.js
@@ -1,27 +1,25 @@
-'use server';
+"use server";
 
-import { pool } from './config';
-import 'mssql'
-import sql from 'mssql';
+import { pool } from "./config";
+import "mssql";
+import sql from "mssql";
 
 export async function GetAllCourses() {
-    try {
-        await pool.connect();
-        const result = await pool.request().query('SELECT * FROM Course');
-        return result.recordset;
-    } catch (err) {
-        console.error('SQL error: ', err);
-    }
+  try {
+    await pool.connect();
+    const result = await pool.request().query("SELECT * FROM Course");
+    return result.recordset;
+  } catch (err) {
+    console.error("SQL error: ", err);
+  }
 }
 
-
-
 export async function GetCourseBySemAndYr(semester, year) {
-    try {
-        await pool.connect();
+  try {
+    await pool.connect();
 
-        // Define SQL query with placeholders
-        const query = `
+    // Define SQL query with placeholders
+    const query = `
             SELECT * 
             FROM CourseCatalog as CC JOIN Course
             ON (CC.CRN = Course.CRN)
@@ -30,15 +28,40 @@ export async function GetCourseBySemAndYr(semester, year) {
             AND CC.RecommendedYear = @Year;
         `;
 
-        // Execute the query with parameters
-        const result = await pool.request()
-            .input('CatalogID', sql.Int, 1)
-            .input('Semester', sql.VarChar, semester)
-            .input('Year', sql.Int, year)
-            .query(query);
+    // Execute the query with parameters
+    const result = await pool
+      .request()
+      .input("CatalogID", sql.Int, 1)
+      .input("Semester", sql.VarChar, semester)
+      .input("Year", sql.Int, year)
+      .query(query);
 
-        return result.recordset;
-    } catch (err) {
-        console.error('SQL error: ', err);
-    }
+    return result.recordset;
+  } catch (err) {
+    console.error("SQL error: ", err);
+  }
+}
+
+export async function GetCatalog(catalogID) {
+  try {
+    await pool.connect();
+
+    // Define SQL query with placeholders
+    const query = `
+        SELECT * 
+        FROM CourseCatalog as CC JOIN Course
+        ON (CC.CRN = Course.CRN)
+        WHERE CC.CatalogID = @CatalogID
+        `;
+
+    // Execute the query with parameters
+    const result = await pool
+      .request()
+      .input("CatalogID", sql.Int, catalogID)
+      .query(query);
+
+    return result.recordset;
+  } catch (err) {
+    console.error("SQL error: ", err);
+  }
 }
