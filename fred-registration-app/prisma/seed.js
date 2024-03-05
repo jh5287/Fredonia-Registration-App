c57b5d45dec2ec08943e834b19a519b87bad4c80
Camron Walsh <camronwalsh@gmail.com>
2024-03-04 20:15:58 -0500

    Created Prisma Seed Script

---
+++ fred-registration-app/prisma/seed.js
@@ -0,0 +1,29 @@
+const { PrismaClient } = require('@prisma/client');
+const fs = require('fs');
+const path = require('path');
+
+const prisma = new PrismaClient();
+
+// Paths to SQL scripts
+const sqlFileNames = ['FRDCreateTables.sql', 'FRDBaseData.sql', 'FRDUserTestData.sql' ,'FRDCatalogTestData.sql'];
+const sqlFilePaths = sqlFileNames.map(fileName => path.join(__dirname, '../database', fileName));
+
+async function main() {
+  for (const filePath of sqlFilePaths) {
+    // Read the SQL file content
+    const sql = fs.readFileSync(filePath, 'utf8');
+
+    // Execute the SQL script
+    await prisma.$executeRawUnsafe(sql);
+    console.log(`Executed SQL script: ${filePath}`);
+  }
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
