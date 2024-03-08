dc0bb589bc026fd25989e43d18e933a5abc90490
Jared Russell <jaredhrussell2016@gmail.com>
2024-03-08 11:17:16 -0500

    Create new branch and add new what if folder

--- fred-registration-app/src/app/what-if/page.jsx
+++ fred-registration-app/src/app/what-if/page.jsx
@@ -1,183 +1,150 @@
 "use client";
 import { useState, useEffect } from "react";
 import Semester from "@/components/Semester";
-import { cn } from "@/lib/utils";
-import Icon from '@mdi/react';
-import { mdiProgressHelper } from '@mdi/js';
+import AcademicSummaryBanner from "@/components/AcademicSummary";
+import { useSession } from "next-auth/react";
+import { FaCheckCircle, FaTimesCircle, FaUserCheck, FaRegCircle } from "react-icons/fa";
 
-const WhatIfSemester = ({ number, data, catalog }) => {
-  const [currentCourses, setCurrentCourses] = useState(Array(data.length).fill(''));//state to hold the current course
+const RoadMap = () => {
+  const [catalog, setCatalog] = useState([]);
+  const [userCourses, setUserCourses] = useState(null);
+  const [userCGPA, setUserCGPA] = useState(null);
+  const { data: session, status } = useSession();
 
-  const handleCourseChange = (e, index) => {
-    const course = e.target.value;
-    setCurrentCourses(prevCourses => {
-      const newCourses = [...prevCourses];
-      newCourses[index] = course;
-      return newCourses;
-    });
+  // Fetch catalog data
+  const fetchCatalog = async () => {
+    try {
+      const res = await fetch("/api/catalog?catID=1");
+      const data = await res.json();
+      setCatalog(data);
+    } catch (err) {
+      console.error("Failed to fetch catalog:", err);
+    }
   };
-  const calculateGPA = (data) => {
-    let totalCredits = 0;
-    let totalPoints = 0;
-    data.forEach((item) => {
-      totalCredits += item.Course.Credits;
-      switch (item.Grade) {
-        case 'A':
-          totalPoints += 4 * item.Course.Credits;
-          break;
-        case 'A-':
-          totalPoints += 3.7 * item.Course.Credits;
-          break;
-        case 'B+':
-          totalPoints += 3.3 * item.Course.Credits;
-          break;
-        case 'B':
-          totalPoints += 3 * item.Course.Credits;
-          break;
-        case 'B-':
-          totalPoints += 2.7 * item.Course.Credits;
-          break;
-        case 'C+':
-          totalPoints += 2.3 * item.Course.Credits;
-          break;
-        case 'C':
-          totalPoints += 2 * item.Course.Credits;
-          break;
-        case 'C-':
-          totalPoints += 1.7 * item.Course.Credits;
-          break;
-        case 'D+':
-          totalPoints += 1.3 * item.Course.Credits;
-          break;
-        case 'D':
-          totalPoints += 1 * item.Course.Credits;
-          break;
-        case 'D-':
-          totalPoints += 0.7 * item.Course.Credits;
-          break;
-        case 'S':
-          totalCredits -= item.Course.Credits;
-          break;
-        case 'WC':
-          totalCredits -= item.Course.Credits;
-          break;
-        default:
-          totalPoints += 0;
-      }
-    });
-    return (totalPoints / totalCredits).toFixed(2);
-  }
 
+  // Fetch user course data
+  const fetchUserCourses = async () => {
+    try {
+      const userEmail = "wals9256@fredonia.edu";
+      const response = await fetch(
+        `/api/student/studentCourses?email=${userEmail}`
+      );
+      const data = await response.json();
+      setUserCourses(data);
+    } catch (error) {
+      console.error("Failed to fetch user profile:", error);
+    }
+  };
+
+  // Fetch user CGPA data
+  const fetchUserCGPA = async () => {
+    try {
+      const userEmail = "wals9256@fredonia.edu";
+      const response = await fetch(`/api/student/CGPA?email=${userEmail}`);
+      const data = await response.json();
+  
+      if (Array.isArray(data) && data.length > 0) {
+        const userCGPAData = data[0];
+        
+        // Try and convert CGPA to a number
+        const cgpa = parseFloat(userCGPAData.CGPA);
+  
+        if (!isNaN(cgpa)) { // Check if conversion was successful
+          setUserCGPA(cgpa);
+        } else {
+          // Handle case where CGPA is not a valid number
+          console.log("CGPA is not a valid number.");
+          setUserCGPA(null);
+        }
+      } else {
+        console.log("No CGPA data found for the user.");
+        setUserCGPA(null);
+      }
+    } catch (error) {
+      console.error("Failed to fetch user profile:", error);
+      setUserCGPA(null); // Ensure user CGPA is set to null in case of error
+    }
+  };
+  
+
+  useEffect(() => {
+    fetchCatalog();
+    fetchUserCourses();
+    fetchUserCGPA();
+  }, []);
+
+  // Filter catalog by year and semester
+  const filterCatalogCourses = (year, semester) => {
+    return catalog.filter(
+      (course) =>
+        course.RecommendedYear === year &&
+        course.RecommendedSemester === semester
+    );
+  };
+
+  // Filter user courses by a semester catalog
+  const filterUserCoursesForSemester = (filteredCatalogCourses) => {
+    if (!Array.isArray(userCourses) || userCourses.length === 0) {
+      return [];
+    }
+
+    return userCourses.filter((userCourse) =>
+      filteredCatalogCourses.some(
+        (catalogCourse) => catalogCourse.CRN === userCourse.CRN
+      )
+    );
+  };
 
   return (
     <>
-    <div className="">
+      <div className="p-3">
+        <AcademicSummaryBanner cgpa={userCGPA} />
+        <div className="flex flex-col items-center">
+          <h1 className="py-5 text-2xl">Computer Science Roadmap</h1>
+          <div className="flex flex-row">
+            <div className="flex flex-row items-center mx-2">
+              <FaCheckCircle color="green" />
+              <p>=Completed</p>
+            </div>
+            <div className="flex flex-row items-center mx-2">
+              <FaTimesCircle color="red" />
+              <p>=Incomplete</p>
+            </div>
+            <div className="flex flex-row items-center mx-2">
+              <FaUserCheck color="blue" />
+              <p>=Enrolled</p>
+            </div>
+            <div className="flex flex-row items-center mx-2">
+              <FaRegCircle />
+              <p>=Not Taken</p>
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 gap-5 h-full md:grid-cols-2">
+          {Array.from({ length: 8 }, (_, i) => {
+            const year = Math.ceil((i + 1) / 2);
+            const semesterStr = i % 2 === 0 ? "Fall" : "Spring";
+            const semesterCatalogCourses = filterCatalogCourses(
+              year,
+              semesterStr
+            );
+            const semesterUserCourses = filterUserCoursesForSemester(
+              semesterCatalogCourses
+            );
 
-      <div className="flex justify-between items-center">
-        <h1 className="py-2 pl-1 text-lg font-medium">{data[0]?.Term.Semester + " " + data[0]?.Term.Year}</h1>
-        {calculateGPA(data) > 0.0 ? <span className="text-base">GPA: {calculateGPA(data)}</span> : null} {/*if there is no applicable grade do not display it */}
-      </div>
-      
-      <div className="border rounded">
-        <table className="table">
-          <thead>
-            <tr>
-              <th className="whitespace-nowrap">Course Code</th>
-              <th>Course Title</th>
-              <th>Credits</th>
-              <th>Grade</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((item, index) => (
-              <tr key={index} className={cn({" bg-red-200" : item.Grade === 'F', 'bg-green-200': item.Grade === 'A'}, )}>
-                <td>{currentCourses[index] === '' ? item.Course.CourseCode : currentCourses[index]}</td>
-                <td>
-                    <select className="select select-primary w-full" onChange={(e) => handleCourseChange(e, index)}>
-                        <option selected disabled>{item.Course.Title}</option>
-                        {catalog.map((course, index) => (
-                            <option key={index} value={course.CourseCode}>{course.Title}</option>
-                        ))}
-                    </select>
-                </td>
-                <td>{item.Course.Credits}</td>
-
-                {/* If the grade is null, display a progress icon, else display the grade */}
-                <td className={cn({"text-red-600" : item.Grade === 'F', 'text-green-600': item.Grade === 'A'}, )}>
-                    { item.Grade === null ? <span className="tooltip" data-tip="In Progress..."><Icon path={mdiProgressHelper} title="Progress" size={1} color="blue" /></span> : 
-                    item.Grade}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            return (
+              <Semester
+                key={i + 1}
+                number={i + 1}
+                catalogData={semesterCatalogCourses}
+                userCourses={semesterUserCourses}
+              />
+            );
+          })}
+        </div>
       </div>
     </>
   );
 };
 
-const WhatIf = () => {
-  const [studentData, setStudentData] = useState([]);
-  const [catalog, setCatalog] = useState([]);
-  
-
-  useEffect(() => {
-    const fetchStudentData = async () => {
-      try {
-        const res = await fetch("/api/student/studentCourses?email=russ9214@fredonia.edu");
-        const studentData = await res.json();
-        const terms = studentData.map(item => (item.Term.Semester + " "+ item.Term.Year)).filter((value, index, self) => self.indexOf(value) === index);//get all the unique terms for the selected student
-        const organized_data = []
-        console.log("Terms: ", terms);
-        console.log("Student data: ", studentData);
-      for(let i = 1; i < terms.length; i++) {
-        const termToCompareTo = terms[i]; //get the term to compare to
-        const semData = studentData.filter((item) => (item.Term.Semester + " " + item.Term.Year) === termToCompareTo);//filter the data to only include the target term
-        organized_data.push(semData)
-      }
-      console.log("Organized data for current reg: ", organized_data);
-      setStudentData(organized_data);
-      } catch (err) {
-        console.error("Failed to fetch student data:", err);
-      }
-    };
-    const fetchCatalog = async () => {
-        try {
-          const res = await fetch("/api/catalog?catID=1");
-          const data = await res.json();
-          console.log("fetched catalog data: ", data);
-          const catalog = [];
-          for (let i = 0; i < data.length; i++) {
-            let temp_data = {
-                CourseCode: data[i].Course.CourseCode,
-                Title: data[i].Course.Title,
-                Credits: data[i].Course.Credits
-            }
-            catalog.push(temp_data);
-          }
-          console.log("Organized catalog: ", catalog);
-          setCatalog(catalog);
-        } catch (err) {
-          console.error("Failed to fetch catalog:", err);
-        }
-      };
-
-    fetchCatalog();
-    fetchStudentData();
-  }, []);
-
-
-
-  return (
-    <>
-    <h1 className="p-3 py-5 text-2xl">What If View</h1>
-    <div className="m-3 grid grid-cols-1 gap-8 h-full md:grid-cols-2">
-    {studentData.map((item, index) => (
-            <WhatIfSemester key={index+1} number={index} data={item} catalog={catalog}/>
-          ))}
-    </div>
-  </>
-  )
-}
-
-export default WhatIf
+export default RoadMap;
