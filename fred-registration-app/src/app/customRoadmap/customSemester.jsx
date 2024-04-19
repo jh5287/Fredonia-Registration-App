1536f9e232112ff4e1d85a5fa19f17105863ded7
Camron Walsh <camronwalsh@gmail.com>
2024-04-19 12:37:31 -0400

    Drag and Drop Styling

--- fred-registration-app/src/app/customRoadmap/customSemester.jsx
+++ fred-registration-app/src/app/customRoadmap/customSemester.jsx
@@ -1,36 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDrop } from "react-dnd";
-
-const ItemTypes = {
-  COURSE: "dndCourse",
-};
-
-function test(droppedItem) {
-  console.log("SUCESSFUL DROP", droppedItem);
-}
+import { ItemTypes } from "@/components/CourseSearch";
 
 export default function CustomSemester({ semesterNum }) {
   const [courses, setCourses] = useState([]);
+  const [justDropped, setJustDropped] = useState(false);
 
   const [{ isOver }, drop] = useDrop(() => ({
     accept: ItemTypes.COURSE,
     drop: (item, monitor) => {
       const droppedCourse = monitor.getItem();
       setCourses((prevArray) => [...prevArray, droppedCourse]);
+      setJustDropped(true);
     },
     collect: (monitor) => ({
       isOver: !!monitor.isOver(),
     }),
   }));
 
+  useEffect(() => {
+    if (justDropped) {
+      const timer = setTimeout(() => {
+        setJustDropped(false);
+      }, 400);
+      return () => clearTimeout(timer);
+    }
+  });
+
   return (
     <>
       <div
-        className={`border-2 rounded-md p-2   ${
+        className={`border-2 rounded-md p-2 transition-colors ${
           isOver
-            ? "border-primary transition ease-in-out delay-75 duration-300"
-            : " "
-        }`}
+            ? "border-primary  ease-in-out delay-75 duration-300  "
+            : ""
+        } ${justDropped ? "border-blue-100 duration-500 " : ""}`}
         ref={drop}
       >
         <h1 className="w-full py-2 pl-2 text-lg ">
