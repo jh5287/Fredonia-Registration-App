a667296d898752f5b63b73fe8c404a6f0498ff78
Camron Walsh <camronwalsh@gmail.com>
2024-04-19 14:02:17 -0400

    Drop Sucess Styling

--- fred-registration-app/src/app/customRoadmap/customSemester.jsx
+++ fred-registration-app/src/app/customRoadmap/customSemester.jsx
@@ -2,39 +2,59 @@ import { useEffect, useState } from "react";
 import { useDrop } from "react-dnd";
 import { ItemTypes } from "@/components/CourseSearch";
 
+
 export default function CustomSemester({ semesterNum }) {
   const [courses, setCourses] = useState([]);
   const [justDropped, setJustDropped] = useState(false);
+  const [dropSuccess, setDropSuccess] = useState(true);
 
-  const [{ isOver }, drop] = useDrop(() => ({
-    accept: ItemTypes.COURSE,
-    drop: (item, monitor) => {
-      const droppedCourse = monitor.getItem();
-      setCourses((prevArray) => [...prevArray, droppedCourse]);
-      setJustDropped(true);
-    },
-    collect: (monitor) => ({
-      isOver: !!monitor.isOver(),
+  const [{ isOver }, drop] = useDrop(
+    () => ({
+      accept: ItemTypes.COURSE,
+      drop: (item, monitor) => {
+        const droppedCourse = monitor.getItem();
+        const CourseAlreadyExists = courses.some(
+          (course) => course.CRN === droppedCourse.CRN
+        );
+
+        if (!CourseAlreadyExists) {
+          setDropSuccess(true);
+          setCourses((prevArray) => [...prevArray, droppedCourse]);
+        } else {
+          setDropSuccess(false);
+        }
+
+        setJustDropped(true);
+      },
+      collect: (monitor) => ({
+        isOver: !!monitor.isOver(),
+      }),
     }),
-  }));
+    [courses]
+  );
 
   useEffect(() => {
     if (justDropped) {
       const timer = setTimeout(() => {
+        console.log("HERE"); 
         setJustDropped(false);
       }, 400);
       return () => clearTimeout(timer);
     }
-  });
+  },[ justDropped, dropSuccess]);
 
   return (
     <>
       <div
-        className={`border-2 rounded-md p-2 transition-colors ${
-          isOver
-            ? "border-primary  ease-in-out delay-75 duration-300  "
+        className={`border-2 rounded-md p-2 transition-colors duration-300 ${
+          isOver ? "border-primary ease-in-out delay-75" : ""
+        } ${
+          justDropped
+            ? dropSuccess
+              ? "border-blue-200 delay-75"
+              : "border-red-400"
             : ""
-        } ${justDropped ? "border-blue-100 duration-500 " : ""}`}
+        }`}
         ref={drop}
       >
         <h1 className="w-full py-2 pl-2 text-lg ">
