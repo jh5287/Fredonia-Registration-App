0885930f0f3c8b957b4f0016b93b2ffecd559793
SquishyMona <kotonefemc@gmail.com>
2024-04-29 09:10:29 -0400

    bringing back camrons changes

--- fred-registration-app/src/app/customRoadmap/page.jsx
+++ fred-registration-app/src/app/customRoadmap/page.jsx
@@ -1,16 +1,21 @@
 "use client";
 import { useState } from "react";
+import { BsArrowBarLeft, BsXLg } from "react-icons/bs";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 import CustomSemester from "./CustomSemester";
-import CourseSearch from "@/components/courseSearch";
-import { BsArrowBarLeft, BsArrowBarRight } from "react-icons/bs";
+import CourseSearch from "@/components/CourseSearch";
 
-function Sidebar({}) {
+function CourseSearchSidebar({}) {
   const [expanded, setExpanded] = useState(true);
 
   return (
     <>
       {!expanded ? (
-        <button onClick={() => setExpanded((curr) => !curr)} className="object-left-top">
+        <button
+          onClick={() => setExpanded((curr) => !curr)}
+          className="object-left-top"
+        >
           <BsArrowBarLeft className="size-8" />
         </button>
       ) : (
@@ -18,9 +23,16 @@ function Sidebar({}) {
       )}
       <aside className={`${expanded ? "w-1/2" : "w-0"}`}>
         <nav className={`h-full ${expanded ? " shadow-md px-2" : ""}`}>
-          <div className={`overflow-hidden h-full ${expanded ? "w-full" : "w-0"}`}>
-            <button onClick={() => setExpanded((curr) => !curr)}>
-              <BsArrowBarRight className="size-8" />
+          <div
+            className={`relative overflow-hidden h-full ${
+              expanded ? "w-full" : "w-0"
+            }`}
+          >
+            <button
+              onClick={() => setExpanded((curr) => !curr)}
+              className="absolute top-0 right-0 py-2"
+            >
+              <BsXLg className="size-5" />
             </button>
             <CourseSearch />
           </div>
@@ -31,37 +43,33 @@ function Sidebar({}) {
 }
 
 export default function customRoadmap() {
-  const createDefaultCourse = () => ({
-    courseCode: "",
-    courseTitle: "",
-    credits: "",
-  });
-
   const createDefaultSemester = (semesterNum) => ({
     semesterNum,
-    courses: Array.from({ length: 5 }, (_, index) => createDefaultCourse()),
+    courses: [],
   });
 
   const [semestersData, setSemesterData] = useState(
     Array.from({ length: 8 }, (_, index) => createDefaultSemester(index + 1))
   );
-
   return (
     <>
-      <div className="flex flex-row gap-x-4 h-screen w-full">
-        <div className="relative flex-1 overflow-y-auto">
-          <div className="grid grid-cols-1 gap-5 h-full py-2">
-            {semestersData.map((semester) => (
-              <CustomSemester
-                key={semester.semesterNum}
-                semesterNum={semester.semesterNum}
-                courses={semester.courses}
-              />
-            ))}
+      <DndProvider backend={HTML5Backend}>
+        <div className="flex flex-row gap-x-4 h-screen w-full ">
+          <div className="relative flex-1 overflow-y-auto px-2">
+            <h1 className={"text-lg pt-1 pb-2"}>Custom Roadmap</h1>
+            <div className="grid grid-cols-1 gap-5 h-full ">
+              {semestersData.map((semester, index) => (
+                  <CustomSemester
+                    key={semester.semesterNum || index}
+                    semesterNum={semester.semesterNum}
+                    courses={semester.courses}
+                  />
+              ))}
+            </div>
           </div>
+          <CourseSearchSidebar/>
         </div>
-        <Sidebar />
-      </div>
+      </DndProvider>
     </>
   );
 }
