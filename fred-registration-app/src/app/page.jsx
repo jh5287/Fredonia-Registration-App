0927a8fb233c66e0048312e08ec15f46df2715d8
Camron Walsh <camronwalsh@gmail.com>
2024-05-06 11:27:51 -0400

    Removed Text Wrap on Main Tabs

--- fred-registration-app/src/app/page.jsx
+++ fred-registration-app/src/app/page.jsx
@@ -9,22 +9,22 @@ const Page = () => {
   return (
     <div className="m-3 relative">
       <div role="tablist" className="tabs tabs-bordered  w-full">
-        <input type="radio" name="home_tabs" role="tab" className="tab border-b-white" aria-label="Registration" defaultChecked/>
+        <input type="radio" name="home_tabs" role="tab" className="tab text-nowrap border-b-white" aria-label="Registration" defaultChecked/>
         <div role="tabpanel" className="tab-content p-10">
           <Registration />
         </div>
 
-        <input type="radio" name="home_tabs" role="tab" className="tab" aria-label="Roadmap" />
+        <input type="radio" name="home_tabs" role="tab" className="tab text-nowrap" aria-label="Roadmap" />
         <div role="tabpanel" className="tab-content p-10">
           <RoadMap />
         </div>
 
-        <input type="radio" name="home_tabs" role="tab" className="tab" aria-label="Future Plan" />
+        <input type="radio" name="home_tabs" role="tab" className="tab text-nowrap" aria-label="Future Plan" />
         <div role="tabpanel" className="tab-content p-10">
           <FuturePlan />
         </div>
 
-        <input type="radio" name="home_tabs" role="tab" className="tab" aria-label="Advisor Chat" />
+        <input type="radio" name="home_tabs" role="tab" className="tab text-nowrap" aria-label="Advisor Chat" />
         <div role="tabpanel" className="tab-content p-10">
           <AdvisorChat role={'student'} />
         </div>
