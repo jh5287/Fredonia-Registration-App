69faadbc160227d5edb6cbec8091ceca98f2cc31
Camron Walsh <camronwalsh@gmail.com>
2024-05-08 09:10:30 -0400

    Fixed Roadmap Builder Page Width

--- fred-registration-app/src/app/customRoadmap/page.jsx
+++ fred-registration-app/src/app/customRoadmap/page.jsx
@@ -83,7 +83,7 @@ export default function customRoadmap() {
   return (
     <>
       <DndProvider backend={HTML5Backend}>
-        <div className="flex flex-row gap-x-4 h-screen w-full bg-base-100 rounded-lg m-3 p-3 pb-8">
+        <div className="flex flex-row gap-x-4 h-screen bg-base-100 rounded-lg m-3 p-3 pb-8">
           <div className="relative flex-1 overflow-y-auto px-2">
             <h1 className={"text-lg pt-1 pb-2"}>Custom Roadmap</h1>
             <div className="grid grid-cols-1 gap-5 h-full ">
